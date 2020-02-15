eb018e0e8d40d6c50d8016257a11404034d39665
Ethan Tucker <ectucker1@gmail.com>
2020-02-15 11:41:11 -0600

    Glow effects and more map layout

--- Assets/Maps/sunset.tsx
+++ Assets/Maps/sunset.tsx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.3.2" name="sunset" tilewidth="32" tileheight="32" tilecount="4" columns="0">
+<tileset version="1.2" tiledversion="1.3.2" name="sunset" tilewidth="32" tileheight="32" tilecount="16" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="0">
   <image width="32" height="32" source="../Tiles/balconyEdge.png"/>
@@ -15,8 +15,86 @@
  </tile>
  <tile id="2">
   <image width="32" height="32" source="../Tiles/roofEdge.png"/>
+  <objectgroup draworder="index" id="2">
+   <object id="2" x="0" y="20" width="26" height="8"/>
+  </objectgroup>
  </tile>
  <tile id="3">
   <image width="32" height="32" source="../Tiles/roofMiddle.png"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="20" width="32" height="9"/>
+  </objectgroup>
+ </tile>
+ <tile id="5">
+  <image width="32" height="32" source="../Tiles/floorCracked.png"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="6">
+  <image width="32" height="32" source="../Tiles/floorLittleGrass.png"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="7">
+  <image width="32" height="32" source="../Tiles/floorLotGrass.png"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="8">
+  <image width="32" height="32" source="../Tiles/wallBlank.png"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="9">
+  <image width="32" height="32" source="../Tiles/wallFront.png"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="10">
+  <image width="32" height="32" source="../Tiles/windowBlank.png"/>
+  <objectgroup draworder="index" id="2">
+   <object id="4" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="11">
+  <image width="32" height="32" source="../Tiles/windowFront.png"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="13">
+  <image width="32" height="32" source="../Tiles/floor.png"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="14">
+  <image width="32" height="32" source="../Tiles/wallFront - Copy.png"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="15">
+  <image width="32" height="32" source="../Tiles/windowFront - Copy.png"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="16">
+  <image width="32" height="32" source="../Tiles/balconyEdgeMirror.png"/>
+  <objectgroup draworder="index" id="2">
+   <object id="2" x="7" y="27" width="25" height="5"/>
+  </objectgroup>
+ </tile>
+ <tile id="17">
+  <image width="32" height="32" source="../Tiles/RoofEdgeMirror.png"/>
+  <objectgroup draworder="index" id="2">
+   <object id="3" x="6" y="20" width="26" height="8"/>
+  </objectgroup>
  </tile>
 </tileset>
