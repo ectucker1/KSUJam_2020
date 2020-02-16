ec8db633b110a5629d87980f592fd265471790d9
Ethan Tucker <ectucker1@gmail.com>
2020-02-15 20:04:16 -0600

    Added more to main menu

--- Stages/LoadHazards.gd
+++ Stages/LoadHazards.gd
@@ -6,13 +6,6 @@ var lazer_scene = preload("res://Stages/Lazer.tscn")
 func post_import(scene):
 	for node in scene.get_children():
 		if node is TileMap:
+			#var bounds = get_used_rect()
 			pass
-		elif node is Node2D:
-			for object in node.get_children():
-				if object.name.begins_with("lazer"):
-					var lazer = lazer_scene.instance()
-					lazer.position = object.position + Vector2(16, 48)
-					
-					object.add_child(lazer)
-					lazer.set_owner(scene)
 	return scene
