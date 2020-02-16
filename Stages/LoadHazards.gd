729741fbaa4b255d2cf7b23ac0cf0abf6c10f8fb
Ethan Tucker <ectucker1@gmail.com>
2020-02-15 20:36:14 -0600

    Dynamic camera limit

--- Stages/LoadHazards.gd
+++ Stages/LoadHazards.gd
@@ -6,6 +6,8 @@ var lazer_scene = preload("res://Stages/Lazer.tscn")
 func post_import(scene):
 	for node in scene.get_children():
 		if node is TileMap:
-			#var bounds = get_used_rect()
-			pass
+			var bounds = node.get_used_rect()
+			node.add_to_group("map")
+			node.set_meta("start", bounds.position * 32.0 + Vector2(0.0, 32.0))
+			node.set_meta("end", bounds.end * 32.0 - Vector2(0.0, 32.0))
 	return scene
